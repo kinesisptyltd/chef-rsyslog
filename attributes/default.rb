a3de09650b4910d3df28193e2ba7c776000fc075
Christopher Chow <christopher.chow@kinesis.org>
2016-06-09 14:45:58 +1000

    chore(papertail): Update checksum

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default["rsyslog"]["papertrail"] = {
   "enabled" => true,
   "tls" => false,
   "ca_bundle" => "https://papertrailapp.com/tools/papertrail-bundle.pem",
-  "ca_bundle_checksum" => "c03a504397dc45b4fc05f978dbf02129793cbd2a0b64856c2ba1bb49a3b9aacb"
+  "ca_bundle_checksum" => "7019189e20ed695e9092e67d91a3b37249474f4c4c6355193ce6d2cb648f147c"
 }
